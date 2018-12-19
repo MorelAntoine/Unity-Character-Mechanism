aa7399fd6f25d6dbf9cdd2dd93a1bdf421afd603
MorelAntoine <antoinemorel@outlook.com>
2018-12-19 11:18:44 -0500

    Add Locomotion Profiles attributes

--- Sources/System/Profile/Locomotion/LocomotionProfile2D.cs
+++ Sources/System/Profile/Locomotion/LocomotionProfile2D.cs
@@ -1,4 +1,6 @@
-﻿namespace UniCraft.CharacterMechanism.System.Profile.Locomotion
+﻿using UnityEngine;
+
+namespace UniCraft.CharacterMechanism.System.Profile.Locomotion
 {
     /// <summary>
     /// Profile to store all the 2D locomotion settings
@@ -6,5 +8,6 @@
     [global::System.Serializable]
     public sealed class LocomotionProfile2D
     {
+        [Range(1f, 6f)] public float WalkSpeed = 2f;
     }
 }
--- Sources/System/Profile/Locomotion/LocomotionProfile3D.cs
+++ Sources/System/Profile/Locomotion/LocomotionProfile3D.cs
@@ -1,4 +1,6 @@
-﻿namespace UniCraft.CharacterMechanism.System.Profile.Locomotion
+﻿using UnityEngine;
+
+namespace UniCraft.CharacterMechanism.System.Profile.Locomotion
 {
     /// <summary>
     /// Profile to store all the 3D locomotion settings
@@ -6,5 +8,24 @@
     [global::System.Serializable]
     public sealed class LocomotionProfile3D
     {
+        ///////////////////////////////
+        ////////// Attribute //////////
+        ///////////////////////////////
+        
+        [Range(90f, 130)] public float AngularSpeed = 120f;
+        [Range(140f, 180f)] public float AngularSpeedStationary = 160f;
+        [Range(1f, 6f)] public float WalkSpeed = 2f;
+        
+        ////////////////////////////
+        ////////// Method //////////
+        ////////////////////////////
+
+        /// <summary>
+        /// Return the right amount of angular speed between AngularSpeedStationary and AngularSpeed 
+        /// </summary>
+        public float InterpolateAngularSpeed(float coefficient)
+        {
+            return (Mathf.Lerp(AngularSpeedStationary, AngularSpeed, coefficient));
+        }
     }
 }
