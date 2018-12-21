6c77319b92907491c0f12c8b9ec0db6e6a75f2e3
MorelAntoine <antoinemorel@outlook.com>
2018-12-21 09:57:36 -0500

    Add State examples

---
+++ Example/Scripts/Motion/State/IdleState.cs
@@ -0,0 +1,21 @@
+﻿using UniCraft.CharacterMechanism.System;
+using UniCraft.CharacterMechanism.System.Motion.Information;
+using UniCraft.CharacterMechanism.System.Motion.StateMachine;
+using UnityEngine;
+
+namespace UniCraft.CharacterMechanism.Example.Motion.State
+{
+    /// <inheritdoc/>
+    /// <summary>
+    /// Basic idle state
+    /// </summary>
+    [CreateAssetMenu(menuName = "UniCraft/Character/Example/State/Idle")]
+    public class IdleState : AMotionState
+    {
+        public override void Begin(ACharacterSystem characterSystem)
+        {}
+
+        public override void Tick(MotionConfiguration mc, MotionInput mi)
+        {}
+    }
+}
