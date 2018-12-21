121363dd6d33d3951af1d726205a3e09a79b7f98
MorelAntoine <antoinemorel@outlook.com>
2018-12-21 09:55:59 -0500

    Add Condition examples

---
+++ Example/Scripts/Motion/Condition/HasDirectionalMovementInputCondition.cs
@@ -0,0 +1,20 @@
+﻿using UniCraft.CharacterMechanism.System;
+using UniCraft.CharacterMechanism.System.Motion.Information;
+using UniCraft.CharacterMechanism.System.Motion.StateMachine;
+using UnityEngine;
+
+namespace UniCraft.CharacterMechanism.Example.Motion.Condition
+{
+    /// <inheritdoc/>
+    /// <summary>
+    /// Verify if there is a directional movement input or not
+    /// </summary>
+    [CreateAssetMenu(menuName = "UniCraft/Character/Example/Condition/HasDirectionalMovementInput")]
+    public class HasDirectionalMovementInputCondition : AMotionCondition
+    {
+        public override bool IsConditionMet(ACharacterSystem characterSystem, MotionInput motionInput)
+        {
+            return (motionInput.MovementDirection != Vector3.zero);
+        }
+    }
+}
---
+++ Example/Scripts/Motion/Condition/HasNoDirectionalMovementInputCondition.cs
@@ -0,0 +1,20 @@
+﻿using UniCraft.CharacterMechanism.System;
+using UniCraft.CharacterMechanism.System.Motion.Information;
+using UniCraft.CharacterMechanism.System.Motion.StateMachine;
+using UnityEngine;
+
+namespace UniCraft.CharacterMechanism.Example.Motion.Condition
+{
+    /// <inheritdoc/>
+    /// <summary>
+    /// Verify if there is no directional movement input or not
+    /// </summary>
+    [CreateAssetMenu(menuName = "UniCraft/Character/Example/Condition/HasNoDirectionalMovementInput")]
+    public class HasNoDirectionalMovementInputCondition : AMotionCondition
+    {
+        public override bool IsConditionMet(ACharacterSystem characterSystem, MotionInput motionInput)
+        {
+            return (motionInput.MovementDirection == Vector3.zero);
+        }
+    }
+}
